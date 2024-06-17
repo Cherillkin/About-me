e78affa304273104ef762b756b45b307820e15fe
Cherillkin <108462023+Cherillkin@users.noreply.github.com>
2024-06-17 14:18:30 +0600

    first-commit

---
+++ components/Experience.tsx
@@ -0,0 +1,43 @@
+import { workExperience } from "@/data";
+import React from "react";
+import { Button } from "./ui/MovingBorders";
+
+const Experience = () => {
+  return (
+    <div className="py-20" id="testimonials">
+      <h1 className="heading">
+        Kind words from
+        <span className="text-purple"> satisfied clients</span>
+      </h1>
+
+      <div className="w-full gap-10 grid-cols-1 lg:grid-cols-4 grid mt-12">
+        {workExperience.map((card) => (
+          <Button
+            key={card.id}
+            borderRadius="1.75rem"
+            duration={Math.floor(Math.random() * 10000) + 10000}
+            className="flex-1 text-white border-neutral-200 dark:border-slate-800"
+          >
+            <div className="flex flex-col lg:flex-row lg:items-center gap-2 p-3 py-6 md:p-5 lg:p-10">
+              <img
+                src={card.thumbnail}
+                alt={card.thumbnail}
+                className="lg:w-32 md:w-20 w-16"
+              />
+              <div className="lg:ms-5">
+                <h1 className="text-start text-xl md:text-2xl font-bold">
+                  {card.title}
+                </h1>
+                <p className="text-start text-white-100 mt-3 font-semibold">
+                  {card.desc}
+                </p>
+              </div>
+            </div>
+          </Button>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Experience;
